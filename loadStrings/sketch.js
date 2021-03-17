fad7cbb2ded0b6466dc4f34654cc70a394997123
erika2201 <erikapapamija@gmail.com>
2021-03-16 19:12:16 -0500

    Código en JS corregido y palabras caen

--- loadStrings/sketch.js
+++ loadStrings/sketch.js
@@ -1,25 +1,32 @@
 let texts;
 let words = [];
+let posi = [];
 function preload(){
   texts = loadStrings('./assets/strings.txt');
 }
 
 function setup() {
   createCanvas(400, 400);
-    background(0);
-  console.log(texts);
-   fill(255);
   for (let index = 0; index < texts.length; index++) {
     let arrayWords = texts[index].split(' '); 
     for (let index = 0; index < arrayWords.length; index++) {
       words.push(arrayWords[index]);
-      text(words[index], random(50,350), random(50,350));
+      let newPosi = {x: random(10,350), y: random(0,50)};
+      posi.push(newPosi);
+    
     }
   }
 
-  console.log(words);
 }
 
 function draw() {
+    background(0);
+  console.log(texts);
+   fill(255);
+for (let index = 0;index < words.length; index++){
+  text(words[index],posi[index].x, posi[index].y);
+  posi[index].y++;
+}
 
+  console.log(words);
 }
