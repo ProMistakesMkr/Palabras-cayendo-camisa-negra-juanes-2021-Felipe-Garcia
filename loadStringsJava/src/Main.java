c8b7a75fa6cf576e2b40fadbff5cea7f56979eec
Santiago Granobles <santiago.granobles@hotmail.com>
2021-03-22 18:28:52 -0500

    Palabras aparecen en lugares random una por una

--- loadStringsJava/src/Main.java
+++ loadStringsJava/src/Main.java
@@ -3,49 +3,67 @@ import java.util.Iterator;
 
 import processing.core.PApplet;
 
-
 public class Main extends PApplet {
-	
+
 	String[] texts;
-	ArrayList<String>words;
+	// ArrayList<String>arrayWords;
+	ArrayList<String> words = new ArrayList<>();
+	String palabra = "";
+	int numero = 0;
+
 
 	public static void main(String[] args) {
 		PApplet.main(Main.class.getName());
-		
+
 	}
 
-	
 	public void settings() {
-		size (400,400);
-		
-	}
+		size(400, 400);
 
+
+	}
 
 	public void setup() {
-		ArrayList<String> words = new ArrayList<>();  
+		frameRate(10);
 		background(0);
 		texts = loadStrings("./data/strings.txt");
 		for (int i = 0; i < texts.length; i++) {
 			String[] arrayWords = texts[i].split(" ");
+			
 			for (int j = 0; j < arrayWords.length; j++) {
-				words.add(arrayWords[j]);
-			}
-
-				}
+				words.add(arrayWords[j]);	
 				
-		for (int k = 0; k < words.size(); k++) {
-			fill (255);
-			textSize(10);
-			text(words.get(k), random(10, 350), random(10, 350));
-			System.out.println(words.get(k));
+
 			}
 		}
-		
 
-	public void draw() {
-		
+			
 		
 		
 	}
 
+	public void draw() {
+		
+
+		
+		if (frameCount % 10 == 0) { 
+			
+			for (int k = 0; k < words.size(); k++) {
+
+					palabra = words.get(numero);
+				
+			}
+			
+			fill(255);
+			textSize(10);
+			text(palabra, random(10, 350), random(10, 350)); 
+			numero ++;
+
+		
+		}
+		
+
+
+
+	}
 }
